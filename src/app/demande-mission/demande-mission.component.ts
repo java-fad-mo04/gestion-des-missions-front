109554dacdb977ffbf38c810eb924f5e540df6bf
Zhanna <j.santybay@gmail.com>
2020-02-25 16:15:22 +0100

    Demande de mission

--- src/app/demande-mission/demande-mission.component.ts
+++ src/app/demande-mission/demande-mission.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
+import { DataService } from '../services/data.service';
+import { AuthService } from '../auth/auth.service';
+
+import { Mission } from '../models/mission';
+import { Nature } from '../models/nature';
+import { Transport } from '../models/transport';
+import { Collegue } from '../auth/auth.domains';
 
 @Component({
   selector: 'app-demande-mission',
@@ -7,11 +15,38 @@ import { Title } from '@angular/platform-browser';
   styles: []
 })
 export class DemandeMissionComponent implements OnInit {
+  isError: boolean;
+  hidden = false;
+  listeNatures: Nature[];
+  natures: Observable<string[]>;
+  listeTransport: Transport[];
+  transports: Observable<string[]>;
+  collegue: Collegue;
+  mission = new Mission(0, new Date(), new Date(), null, '', '', null, '', null);
 
-  constructor(private titleService: Title) { }
+  constructor(private titleService: Title, private dataService: DataService, private _authSrv: AuthService) { }
 
   ngOnInit() {
     this.titleService.setTitle( 'Demande de mission - GDM' );
+
+    this._authSrv.collegueConnecteObs.subscribe(col => {
+      this.collegue = col;
+      }, error => {console.log(error);
+    });
+
+    this.dataService.getNatures().subscribe(natures => {
+      this.listeNatures = natures;
+    }, (error) => {
+      console.log(error);
+    });
+
+  this.dataService.getTransport().subscribe(transports => {
+    this.listeTransport = transports;
+  }, (error) => {console.log(error)});
   }
 
+  creer() {
+    this.mission.collegue = this.collegue;
+    this.dataService.addMission(this.mission).subscribe(() => {}, error => {console.log(`Mission n'a pas été créé`); })
+  }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Mission } from '../models/Mission';
+import { Nature } from '../models/nature';
+import { Transport } from '../models/transport';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +15,17 @@ export class DataService {
 
   getMissions(): Observable<Mission[]> {
     return this.httpClient.get<Mission[]>(`${environment.baseUrl}mission`, {withCredentials: true});
+  }
+
+  addMission(mission: Mission): Observable<Mission>{
+    return this.httpClient.post<Mission>(`${environment.baseUrl}mission`, mission);
+  }
+
+  getNatures(): Observable<Nature[]> {
+    return this.httpClient.get<Nature[]>(`${environment.baseUrl}nature`, {withCredentials: true});
+}
+
+  getTransport(): Observable<Transport[]> {
+    return this.httpClient.get<Transport[]>(`${environment.baseUrl}transport`, {withCredentials: true});
   }
 }
