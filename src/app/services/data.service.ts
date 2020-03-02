524c282b7565a51a430de9a029020eb6e328bf9b
Darcy NGUYEN <nguyendarcy@hotmail.fr>
2020-03-02 13:24:15 +0100

    maj sub visu-nature

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Mission } from '../models/mission';
 import { Nature } from '../models/nature';
@@ -8,48 +8,66 @@ import { Transport } from '../models/transport';
 
 const url = environment.baseUrl;
 
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  natureSubject = new Subject<Nature[]>();
+
+
   constructor(private _httpClient: HttpClient) { }
-// Mission
+  // Mission
   getMissions(): Observable<Mission[]> {
-    return this._httpClient.get<Mission[]>(`${url}mission`, {withCredentials: true});
+    return this._httpClient.get<Mission[]>(`${url}mission`, { withCredentials: true });
   }
 
   getMission(id: number): Observable<Mission[]> {
-    return this._httpClient.get<Mission[]>(`${url}mission/${id}`, {withCredentials: true});
+    return this._httpClient.get<Mission[]>(`${url}mission/${id}`, { withCredentials: true });
   }
 
-  addMission(mission: Mission){
-    return this._httpClient.post<string>(`${url}mission`, mission, {responseType: 'text' as 'json' });
+  addMission(mission: Mission) {
+    return this._httpClient.post<string>(`${url}mission`, mission, { responseType: 'text' as 'json' });
   }
 
   modifierMission(mission: Mission) {
-    return this._httpClient.patch<string>(`${url}mission`, mission, {responseType: 'text' as 'json' });
+    return this._httpClient.patch<string>(`${url}mission`, mission, { responseType: 'text' as 'json' });
   }
 
   deleteMission(id: number) {
     console.log(id);
-    return this._httpClient.delete<string>(`${url}mission/${id}`, {responseType: 'text' as 'json' });
+    return this._httpClient.delete<string>(`${url}mission/${id}`, { responseType: 'text' as 'json' });
   }
-//Nature
+  //Nature
+
+
+  emitListeNat() {
+    let listeNat: Nature[];
+    this.getNatures().subscribe((arg: Nature[]) => {
+      listeNat = arg;
+      console.log(listeNat);
+      this.natureSubject.next(listeNat);
+    });
+  }
+
   getNatures(): Observable<Nature[]> {
-    return this._httpClient.get<Nature[]>(`${url}nature`, {withCredentials: true});
+    return this._httpClient.get<Nature[]>(`${url}nature`, { withCredentials: true });
   }
 
   createNature(nature: Nature) {
+
     return this._httpClient.post<string>(`${url}nature`, nature, { responseType: 'text' as 'json' });
+
   }
 
   modifierNature(nature: Nature) {
     return this._httpClient.patch<string>(`${url}nature`, nature, { responseType: 'text' as 'json' });
   }
-//Transport
+  //Transport
   getTransport(): Observable<Transport[]> {
-    return this._httpClient.get<Transport[]>(`${url}transport`, {withCredentials: true});
+    return this._httpClient.get<Transport[]>(`${url}transport`, { withCredentials: true });
   }
 
 
